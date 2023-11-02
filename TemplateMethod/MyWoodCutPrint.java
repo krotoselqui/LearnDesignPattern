51c675f9c732cf59d2a3dcc912429a622bef9aab
Krotoselqui <dragon0549610@yahoo.co.jp>
2023-11-02 14:19:15 +0900

    Revised concrete method of MyWoodCutPrint

--- TemplateMethod/MyWoodCutPrint.java
+++ TemplateMethod/MyWoodCutPrint.java
@@ -2,29 +2,16 @@ package LearnPattern.TemplateMethod;
 
 public class MyWoodCutPrint extends WoodCutPrint {
 
-    // Reduced Visibility (public -> private) でコンパイルエラー
-    // private void draw(Cuttable hanzai)
-    // {
-    //      System.out.println("版材に描いている振りをしてスマホを見ている");
-    // }
-
-    // オーバーロードはできる
-    public int draw(Cuttable hanzai, int val) {
-        return 10;
-    }
-
     public void draw(Cuttable hanzai) {
-        System.out.println("draw");
+        System.out.println("draw on hanzai");
     }
 
     public void cut(Cuttable hanzai) {
-        System.out.println("cut");
+        System.out.println("carved on hanzai");
     }
 
     public void print(Cuttable hanzai) {
-        String className = new Object() {
-        }.getClass().getEnclosingClass().getName();
-        System.out.println(className);
+        System.out.println("printed using hanzai");
     }
 
 }
