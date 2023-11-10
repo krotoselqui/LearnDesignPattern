9e831c4fc677d3a648ca08f232f9b7afabf5f875
krotoselqui <dragon0549610@yahoo.co.jp>
2023-11-10 21:23:42 +0900

    improved perform using Strategy pattern

---
+++ Strategy/Comparator.java
@@ -0,0 +1,5 @@
+package LearnPattern.Strategy;
+
+public interface Comparator {
+    public int compare(Human h1, Human h2);
+}
--- Strategy/MyClass.java
+++ Strategy/MyClass.java
@@ -1,7 +1,18 @@
 package LearnPattern.Strategy;
 
+import java.util.Comparator;
+
 public class MyClass {
-    public int compare(Human h1,Human h2){
-        return new AgeComparator().compare(h1,h2);
+    // public int compare(Human h1,Human h2){
+    //     return new AgeComparator().compare(h1,h2);
+    // }
+
+    private Comparator comparator = null;
+    public MyClass(Comparator comparator){
+        this.comparator = comparator;
     }
+    public int compare(Human h1,Human h2){
+        return comparator.compare(h1, h2);
+    }
+
 }
