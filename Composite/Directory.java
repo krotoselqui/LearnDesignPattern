5a4ec2322fa0f7e9e2d7f8c95c2e2827cae43175
Krotoselqui <dragon0549610@yahoo.co.jp>
2023-11-13 20:17:05 +0900

    added Directory class

---
+++ Composite/Directory.java
@@ -0,0 +1,7 @@
+package LearnPattern.Composite;
+
+
+
+public class Directory {
+    
+}
--- Composite/File.java
+++ Composite/File.java
@@ -2,5 +2,12 @@ package LearnPattern.Composite;
 
 public class File {
     private String name = null;
-    
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    public void remove() {
+        System.out.println(name + "を削除しました");
+    }
 }
