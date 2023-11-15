9540fa8399410855dbdf7a05fa3491d839144fe2
krotoselqui <dragon0549610@yahoo.co.jp>
2023-11-15 20:46:53 +0900

    added cashewNutsVanillaIcecream with extending VanillaIcecream

---
+++ Decorator/CashewNutsVanillaIcecream.java
@@ -0,0 +1,7 @@
+package LearnPattern.Decorator;
+
+public class CashewNutsVanillaIcecream extends VanillaIcecream{
+    public String getName(){
+        return "カシューナッツバニラアイスクリーム";
+    }
+}
---
+++ Decorator/Test.java
@@ -0,0 +1,10 @@
+package LearnPattern.Decorator;
+
+public class Test {
+
+    public void main(String[] args) {
+        CashewNutsVanillaIcecream cashewNutsVanillaIcecream = new CashewNutsVanillaIcecream();
+        VanillaIcecream vanillaIcecream = new VanillaIcecream();
+        GreenTeaIcecream greenTeaIcecream = new GreenTeaIcecream();
+    }
+}
