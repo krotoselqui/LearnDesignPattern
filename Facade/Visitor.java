9072fa26d88f9ac8f27afa991b7ee4e84be5281d
krotoselqui <dragon0549610@yahoo.co.jp>
2023-11-16 20:06:13 +0900

    modified visitor to use facade

--- Facade/Visitor.java
+++ Facade/Visitor.java
@@ -2,6 +2,8 @@ package LearnPattern.Facade;
 
 public class Visitor {
     public static void main(String[] args){
-        
+        Librarian nakamura = new Librarian();
+        String location = nakamura.searchBook("昆虫図鑑");
+        System.out.println(location + " なのか");
     }
 }
