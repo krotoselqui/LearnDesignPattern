b38bf34e7df73f454b0bac7c0b425ef11aa9dd85
krotoselqui <dragon0549610@yahoo.co.jp>
2023-11-17 16:42:35 +0900

    fixed lovemediator

---
+++ Mediator/LoveMediator.java
@@ -0,0 +1,6 @@
+package LearnPattern.Mediator;
+
+public interface LoveMediator {
+    public void addColleague(Colleague c);
+    public int consulation(Colleague colleagueInLove,Colleague secretLover);
+}
