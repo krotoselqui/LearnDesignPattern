a1d8dcbd8425d6573f9c891c9106820fc7990984
kroto_desktop <dragon0549610@yahoo.co.jp>
2023-11-22 12:23:27 +0900

    changed proxy behavior by time AM/PM

--- Proxy/Fujiwara.java
+++ Proxy/Fujiwara.java
@@ -1,24 +1,34 @@
 package LearnPattern.Proxy;
 
-public class Fujiwara implements Teacher{
+import java.time.LocalDateTime;
+
+public class Fujiwara implements Teacher {
 
     private Teacher _yamada = new Yamada();
 
-    private void printWithAnswerFormat(String st){
+    private void printWithAnswerFormat(String st) {
         System.out.println("それは" + st + "です");
     }
 
-    public void question1(){
+    public void question1() {
         printWithAnswerFormat("解答1(by Fujiwara)");
     }
 
-    public void question2(){
+    public void question2() {
         printWithAnswerFormat("解答2(by Fujiwara)");
     }
 
-    public void question3(){
-        printWithAnswerFormat("山田先生によると、");
-        _yamada.question3();
+    public void question3() {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("今は.." + localDateTime.getHour() + "時ですね。");
+        if (localDateTime.getHour() >= 12) {
+            System.out.println("明日、返答します。");
+        } else {
+            printWithAnswerFormat("山田先生によると、");
+            _yamada.question3();
+        }
+
     }
-    
+
 }
